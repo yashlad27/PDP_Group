beaf904d03eca16ac03a8e27ce95382a2478b10b
Yash Lad <lad.yas@northeastern.edu>
2025-03-02 19:25:50 -0600

    command interface

---
+++ PDPAssignment4/src/controller/command/ICommand.java
@@ -0,0 +1,23 @@
+package controller.command;
+
+/**
+ * Interface for command pattern implementation.
+ * Each command represents a specific operation in the calendar application.
+ */
+public interface ICommand {
+
+  /**
+   * Executes the command with the given arguments.
+   *
+   * @param args the command arguments
+   * @return a string representing the result of command execution
+   */
+  String execute(String[] args);
+
+  /**
+   * Gets the name of the command.
+   *
+   * @return the command name
+   */
+  String getName();
+}
